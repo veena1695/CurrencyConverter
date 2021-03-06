e8dc4969c3578d83f64534b06feb1db18d2b7189
Rohan Marathe <pradip.chalke@zs.com>
2021-03-06 17:31:21 +0530

    login + register done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
+  loginObj :any = {
+    email : '',
+    pass : ''
+  }
+  rememberMe :any = false;
 
   ngOnInit(): void {
   }
 
+  login(){
+    let users = (localStorage.getItem("users")) || "{}";
+    let userObj = JSON.parse(users);
+    let matchFlag = false;
+    userObj.map((y: any )=>{
+      if(y.email == this.loginObj.email && y.pass == this.loginObj.pass){
+        alert("Login Successful");
+        this.navigate('dashboard');
+        matchFlag = true;
+      }
+    })
+    if(!matchFlag){
+      alert("Please check credentials")
+    }
+  }
+
+  navigate(path:any ){
+    this.router.navigateByUrl(path)
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+  regObj : any = {
+    email : '',
+    name : '',
+    pass : ''
+  }
 
   ngOnInit(): void {
   }
 
+  register(){
+    if(this.regObj.email && this.regObj.name && this.regObj.pass){
+      let users = localStorage.getItem("users") || "[{}]"
+      let userObj = JSON.parse(users)
+      if(userObj.length){
+        localStorage.setItem("users", JSON.stringify([...userObj, this.regObj ]))
+      }else{
+        localStorage.setItem("users", JSON.stringify([this.regObj ]))
+      }
+      this.navigate('login')
+    }
+  }
+
+  navigate(path:any ){
+    this.router.navigateByUrl(path)
+  }
 }
